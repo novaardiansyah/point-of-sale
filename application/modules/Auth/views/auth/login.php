aee66978d839cf4469df4f30312af0d003129421
novaardiansyah <novaardiansyah78@gmail.com>
2023-08-07 11:46:07 +0700

    Optimization image

--- application/modules/Auth/views/auth/login.php
+++ application/modules/Auth/views/auth/login.php
@@ -36,7 +36,7 @@
               </div>
             </div>
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('<?= assets_url('img/laura-smetsers-St08jKkPVHw-unsplash.jpg') ?>');
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('<?= assets_url('img/background-login.webp') ?>');
           background-size: cover;">
                 <span class="mask bg-gradient-secondary opacity-7"></span>
                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Embrace the Journey, <br />Chase the Dream"</h4>
