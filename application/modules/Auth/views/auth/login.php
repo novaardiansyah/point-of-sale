0af824dad8c4b06d13327b518902f5b726687c94
novaardiansyah <novaardiansyah78@gmail.com>
2023-08-09 09:51:26 +0700

    Storing login/registration data with Google and sending a welcome email to new users.

--- application/modules/Auth/views/auth/login.php
+++ application/modules/Auth/views/auth/login.php
@@ -6,11 +6,11 @@
           <div class="row">
             <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
               <div class="card card-plain">
-                <div class="card-header pb-0 text-start">
+                <div class="card-header text-start pb-0">
                   <h4 class="font-weight-bolder">Welcome Back!</h4>
                   <p class="mb-0">Enter your email and password to login</p>
                 </div>
-                <div class="card-body">
+                <div class="card-body pb-3">
                   <form role="form">
                     <div class="mb-3">
                       <input type="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email">
@@ -26,6 +26,18 @@
                       <button type="button" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Login</button>
                     </div>
                   </form>
+
+                  <div class="row px-xl-5 px-sm-4 px-3 mt-3">
+                    <div class="mt-2 position-relative text-center">
+                      <p class="text-sm font-weight-bold mb-2 text-secondary text-border d-inline z-index-2 bg-white px-3">
+                        or
+                      </p>
+                    </div>
+
+                    <div class="mt-3">
+                      <?php $this->load->view('auth/auth/components/signup/google-btn'); ?>
+                    </div>
+                  </div>
                 </div>
                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                   <p class="mb-4 text-sm mx-auto">
