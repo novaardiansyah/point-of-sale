195bcd56bbb159b22cf7376a8035e74df9bee53d
novaardiansyah <novaardiansyah78@gmail.com>
2023-08-12 22:20:40 +0700

    README Update

--- application/views/transaksi/pembayaran/list.blade.php
+++ application/views/transaksi/pembayaran/list.blade.php
@@ -4,7 +4,40 @@
   <div class="container">
     <div class="row">
       <div class="col-md">
-        
+        <div class="card">
+          <!-- /.card-header -->
+          <div class="card-body">
+            <button type="button" class="btn btn-sm btn-primary" onclick="return modalAdd()">
+              <i class="fa fa-fw fa-plus"></i> Add
+            </button>
+
+            <div class="table-responsive mt-3">
+              <table id="list_pembayaran" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>No. </th>
+                    <th>Tanggal</th>
+                    <th>No. Nota</th>
+                    <th>Transaksi</th>
+                    <th>Metode Bayar</th>
+                    <th>Jumlah</th>
+                    <th>Bama Bank</th>
+                    <th>No. Rekening</th>
+                    <th>Lampiran</th>
+                    <th>Status</th>
+                    <th>Verifikasi</th>
+                    <th>::</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <!-- from ajax -->
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
       </div>
     </div>
   </div>
